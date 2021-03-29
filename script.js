68b2c3d950a7e9842a3064e00ecfaae5872f858e
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-29 10:09:51 -0400

    started building search section

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var searchForm = $("search-form");
+var searchForm = $("#search-form");
+var searchTermEl = $("#search-term");
 
-searchForm.on("submit", function(){
-
+searchForm.on("submit", function(event) {
+    event.preventDefault();
     //Grab the search term out of the input.
-    //
+    var searchTerm = searchTermEl.val();
+    console.log(searchTerm);
+    //build the API URL with search term and API key
+    //store the API key in a variable
+    //Make an API call using fetch
+    //convert the response from JSON
+    //start by console logging the data
 })
