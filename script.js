d9dbd4fb2f3807986c68f4def207e68a6a75638f
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-29 17:22:38 -0400

    linked to the API

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var searchForm = $("#search-form");
 var searchTermEl = $("#search-term");
 
-searchForm.on("submit", function(event) {
+searchForm.on("submit", function (event) {
     event.preventDefault();
     //Grab the search term out of the input.
     var searchTerm = searchTermEl.val();
+    //.val allows you to pull info from form input
     console.log(searchTerm);
-    //build the API URL with search term and API key
     //store the API key in a variable
+    var apiKey = "32529dfed4bea5888fb05111a3006541";
+    //build the API URL with search term and API key
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey;
     //Make an API call using fetch
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
     //convert the response from JSON
     //start by console logging the data
 })
