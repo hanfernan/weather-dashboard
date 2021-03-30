366d546429da1b7d0e661c1365d2889ede79858b
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-29 23:32:30 -0400

    working on efficiently getting 5 day forecast data

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 var searchForm = $("#search-form");
 var searchTermEl = $("#search-term");
+var weatherContainerEl = $("#weather-container");
 var localWeatherEl = $("#local-weather");
 var fiveDayEl = $("#five-day-forecast");
 var cities = [];
-var today = moment();
 
 searchForm.on("submit", function (event) {
     event.preventDefault();
     //Grab the search term out of the input.
     var searchTerm = searchTermEl.val();
-    // var storageArray = JSON.parse(localStorage.getItem("citiesArray"));
-    // for (i = 0; i < storageArray.length; i++) {
-    //     console.log(storageArray[i])
-    //     if (storageArray[i].toLowerCase() === searchTerm.toLowerCase()) {
-    //         console.log("found city");
-    //         return;
-    //         //write alert "city already entered" in a <p> 
-    //     }
-    // }
     cities.push(searchTerm);
     //convert the response from JSON
     localStorage.setItem("citiesArray", JSON.stringify(cities));
@@ -32,23 +23,47 @@ function getWeather(city) {
     var apiKey = "32529dfed4bea5888fb05111a3006541";
     //build the API URL with search term and API key for local weather
     var localURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+    var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
+
     //Make an API call using fetch
     fetch(localURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            var tempEl = $('<li>').text("Temperature: " + data.main.temp + "F");
-            localWeatherEl.append(tempEl);
-            console.log(data.main.temp);
+            //create today's weather elements and assign content
+            var displayCityName = $('<h2>').addClass('col-sm-12').text(city);
+            //TODO: add today's date with moment to .text(data) above
+            var tempEl = $('<li>').text("Temperature: " + data.main.temp + " °F");
             var humidEl = $('<li>').text("Humidity: " + data.main.humidity + " %");
-            localWeatherEl.append(humidEl);
-            console.log(data.main.humidity);
             var windSpeedEl = $('<li>').text("Wind Speed: " + data.wind.speed + " MPH");
+            //append today's weather elements to the page
+            weatherContainerEl.append(displayCityName);
+            localWeatherEl.append(tempEl);
+            localWeatherEl.append(humidEl);
             localWeatherEl.append(windSpeedEl);
-            console.log(data.wind.speed);
-            // find uv index console.log(data.);
+            //TODO: find uv index console.log(data.);
         });
+    fetch(fiveDayURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (i = 0; i < 37; i + 4) {
+                console.log[data.list[i]]
+            }
+            // var dayOne = data.list[4];
+            // var dayTwo = data.list[12];
+            // var dayThree = data.list[20];
+            // var dayFour = data.list[28];
+            // var dayFive = data.list[36];
+            // console.log(dayOne.dt_txt);
+            // console.log(dayOne.clouds);
+            // console.log(dayOne.main.temp);
+            // console.log(dayOne.main.humidity);
+            // console.log(dayTwo);
+
+        })
 
 
 }
@@ -56,10 +71,24 @@ function getWeather(city) {
 
 function getStorage() {
     if (localStorage.getItem("citiesArray")) {
-        cities = JSON.parse(localStorage.getItem("citiesArray"))
-        //loop through and append to a button for each city that will run getWeather when clicked
+        cities = JSON.parse(localStorage.getItem("citiesArray"));
+        //TODO: loop through and append to a button for each city that will run getWeather when clicked
         console.log(cities);
     }
 }
 
 getStorage();
+
+
+//TODO: Remaining:
+//EASY: find UV index
+//EASY: add today's date with moment.JS
+//MEDIUM: fix formatting 
+//MEDIUM: append search results to buttons that will run getWeather when clicked
+//HARD: append 5 day forecast info to cards and format appropriately 
+//ANNOYING: color code UV index
+
+//QUESTIONS:
+//How do you get those icons on the five day forecast?
+//need a second look: Why is atlanta below?
+//where is UV index??????????
